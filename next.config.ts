a5e3276645e698d485480a10ee025a0d5d36ec8a
beaualbritton <albrittoncb@gmail.com>
2025-10-27 11:08:08 -0400

    fix: packages and prisma updates, exporting prisma client from prisma/index.ts for review CRUD

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
+  //Had errors when loading
+  images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'logo.clearbit.com',
+        },
+      ],
+    }
 };
 
 export default nextConfig;
